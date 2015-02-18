f0530896772c1e25b0fe5372f3406feead68c055
Greg Buckner <gbuckner23@gmail.com>
2015-02-18 11:06:38 -0800

    fixed routes

--- routes.rb
+++ routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   get '/login' => 'sessions#new', as: :login
   post '/login' => 'sessions#create'
-  delete '/login' => 'sessions#destroy', as: :logout
+  get '/logout' => 'sessions#destroy', as: :logout
 
   resources :users
 end
